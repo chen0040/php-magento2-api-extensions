d0181500664f388a6bf97c395292556e410dc54f
xschen <xs0040@gmail.com>
2017-07-25 23:53:26 +0800

    successfully serialize interface

--- magento-modules/chen0040/reviews/Model/ReviewModelInterface.php
+++ magento-modules/chen0040/reviews/Model/ReviewModelInterface.php
@@ -1,45 +1,147 @@
 <?php
+
 namespace chen0040\reviews\Model;
- 
-interface ReviewModelInterface
-{
-	public function getReview_id();
-	
-        public function setReview_id($review_id);
-        
-        public function getCreated_at();
-        
-        public function setCreated_at($created_at);
-	 
-	public function getEntity_id();
-        
-        public function setEntity_id($entity_id);
-        
-        public function getEntity_pk_value();
-        
-        public function setEntity_pk_value($entity_pk_value);
-        
-        public function getStatus_id();
-        
-        public function setStatus_id($status_id);
-        
-        public function getDetail_id();
-        
-        public function setDetail_id($detail_id);
-	
-        public function getTitle();
-        
-        public function setTitle($title);
-        
-        public function getDetail();
-        
-        public function setDetail($detail);
-        
-        public function getNickname();
-        
-        public function setNickname($nickname);
-        
-        public function getCustomer_id();
-        
-        public function setCustomer_id($customer_id);
+
+interface ReviewModelInterface {
+    /**
+     * return the review id
+     * 
+     * @api
+     * @return int review id
+     */
+    public function getReview_id();
+
+    /**
+     * 
+     * @param type $review_id
+     * @return ReviewModelInterface
+     */
+    public function setReview_id($review_id);
+
+    /**
+     * 
+     * @api
+     * @return string created time in string
+     */
+    public function getCreated_at();
+
+    /**
+     * 
+     * @param type $created_at
+     * @return ReviewModelInterface
+     */
+    public function setCreated_at($created_at);
+
+    /**
+     * 
+     * @api
+     * @return int entity id
+     */
+    public function getEntity_id();
+
+    /**
+     * 
+     * @param type $entity_id
+     * @return ReviewModelInterface
+     */
+    public function setEntity_id($entity_id);
+
+    /**
+     * 
+     * @api
+     * @return int
+     */
+    public function getEntity_pk_value();
+
+    /**
+     * 
+     * @param type $entity_pk_value
+     * @return ReviewModelInterface
+     */
+    public function setEntity_pk_value($entity_pk_value);
+
+    /**
+     * 
+     * @api
+     * @return int
+     * @return ReviewModelInterface
+     */
+    public function getStatus_id();
+
+    /**
+     * 
+     * @param type $status_id
+     * @return ReviewModelInterface
+     */
+    public function setStatus_id($status_id);
+
+    /**
+     * 
+     * @api
+     * @return int
+     */
+    public function getDetail_id();
+
+    /**
+     * 
+     * @param type $detail_id
+     * @return ReviewModelInterface
+     */
+    public function setDetail_id($detail_id);
+
+    /**
+     * 
+     * @api
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * 
+     * @param type $title
+     * @return ReviewModelInterface
+     */
+    public function setTitle($title);
+
+    /**
+     * 
+     * @api
+     * @return string
+     */
+    public function getDetail();
+
+    /**
+     * 
+     * @param type $detail
+     * @return ReviewModelInterface
+     */
+    public function setDetail($detail);
+
+    /**
+     * 
+     * @api
+     * @return string
+     */
+    public function getNickname();
+
+    /**
+     * 
+     * @param type $nickname
+     * @return ReviewModelInterface
+     */
+    public function setNickname($nickname);
+
+    /**
+     * 
+     * @api
+     * @return int
+     */
+    public function getCustomer_id();
+
+    /**
+     * 
+     * @param type $customer_id
+     * @return ReviewModelInterface
+     */
+    public function setCustomer_id($customer_id);
 }
